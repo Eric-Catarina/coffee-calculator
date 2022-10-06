2cde1fce65847bc5fd01c3616393b903f4b65b20
Eric-Catarina <2020954200@teiacoltec.org>
2022-10-06 20:33:52 -0300

    feat: Creation of formKey

--- lib/main.dart
+++ lib/main.dart
@@ -24,57 +24,53 @@ class _MyHomePageState extends State<MyHomePage> {
   String _infoText = "Inform your Data";
   String dropdownValue = list.first;
 
-  TextEditingController coffeeController = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
+  TextEditingController coffeeController = TextEditingController();
 
   void _resetTextFields() {
     coffeeController.text = "";
     setState(() {
+      formKey = GlobalKey<FormState>();
       _infoText = "Inform your Data";
     });
   }
 
-  void _calculateCoffe(){
+  void _calculateCoffe() {
     setState(() {
-
       print(list[0]);
 
-      int coffee = int.parse(coffeeController.text) ;
+      int coffee = int.parse(coffeeController.text);
 
-      if(coffee > 400 ){
+      if (coffee > 400) {
         _infoText = "Over Caffeined !";
       }
-      if(dropdownValue == dropdownValue[2]){
+      if (dropdownValue == dropdownValue[2]) {
         //Adults
-        if (coffee < 300){
+        if (coffee < 300) {
           _infoText = "You can get more Coffee!";
-        }
-        else if(coffee < 400 && coffee > 300){
+        } else if (coffee < 400 && coffee > 300) {
           _infoText = "Have Fun !";
         }
       }
-      if(dropdownValue == list[0] || dropdownValue == list[1]){
+      if (dropdownValue == list[0] || dropdownValue == list[1]) {
         //Kids & Teenagers
 
-        if (coffee < 100){
+        if (coffee < 100) {
           _infoText = "You can get more Coffee!";
-        }
-        else if(coffee > 200){
+        } else if (coffee > 200) {
           _infoText = "Over Caffeined !";
-        }
-        else if(coffee < 200 && coffee > 100){
+        } else if (coffee < 200 && coffee > 100) {
           _infoText = "Have Fun !";
         }
       }
-      if(dropdownValue == list[3]){
+      if (dropdownValue == list[3]) {
         //Pregnant
-        if (coffee < 200){
+        if (coffee < 200) {
           _infoText = "You can get more Coffee!";
-        }
-        else if(coffee > 300){
+        } else if (coffee > 300) {
           _infoText = "Over Caffeined !";
-        }
-        else if(coffee < 300 && coffee > 200){
+        } else if (coffee < 300 && coffee > 200) {
           _infoText = "Have Fun !";
         }
       }
@@ -83,15 +79,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
-
-
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text("Coffee Calculator"),
         backgroundColor: Colors.brown,
-        actions: [IconButton(onPressed: _resetTextFields, icon: Icon(Icons.refresh))],
+        actions: [
+          IconButton(onPressed: _resetTextFields, icon: Icon(Icons.refresh))
+        ],
       ),
       body: Center(
         child: Stack(
@@ -111,38 +106,37 @@ class _MyHomePageState extends State<MyHomePage> {
             Container(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-
                 children: [
-                      Padding(
-                      padding: const EdgeInsets.all(30.0),
-                      child: Column(
-
-                        children: [
-                          DropdownButton<String>(
-
-                            value: dropdownValue,
-                            icon: const Icon(Icons.arrow_downward),
-                            elevation: 16,
-                            style: const TextStyle(color: Colors.brown),
-                            underline: Container(
-                              height: 2,
-                              color: Colors.brown,
-                            ),
-                            onChanged: (String? value) {
-                              // This is called when the user selects an item.
-                              setState(() {
-                                dropdownValue = value!;
-                              });
-                            },
-                            items: list
-                                .map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
+                  Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Column(
+                      children: [
+                        DropdownButton<String>(
+                          value: dropdownValue,
+                          icon: const Icon(Icons.arrow_downward),
+                          elevation: 16,
+                          style: const TextStyle(color: Colors.brown),
+                          underline: Container(
+                            height: 2,
+                            color: Colors.brown,
                           ),
-                          TextField(
+                          onChanged: (String? value) {
+                            // This is called when the user selects an item.
+                            setState(() {
+                              dropdownValue = value!;
+                            });
+                          },
+                          items: list
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                        Form(
+                          key: formKey,
+                          child: TextField(
                             controller: coffeeController,
                             keyboardType: TextInputType.number,
                             decoration: InputDecoration(
@@ -152,26 +146,26 @@ class _MyHomePageState extends State<MyHomePage> {
                             textAlign: TextAlign.center,
                             style: TextStyle(color: Colors.brown, fontSize: 30.0),
                           ),
-                          Container(
-                            width: 350,
-                            child: ElevatedButton(
-                              onPressed: _calculateCoffe,
-                              child: Text(
-                                'Calculate',
-                                style:
-                                    TextStyle(fontSize: 24, color: Colors.white),
-                              ),
+                        ),
+                        Container(
+                          width: 350,
+                          child: ElevatedButton(
+                            onPressed: _calculateCoffe,
+                            child: Text(
+                              'Calculate',
                               style:
-                                  ElevatedButton.styleFrom(primary: Colors.brown),
+                                  TextStyle(fontSize: 24, color: Colors.white),
                             ),
+                            style:
+                                ElevatedButton.styleFrom(primary: Colors.brown),
                           ),
-                          Text(
-                            _infoText,
-                            style: TextStyle(color: Colors.brown, fontSize: 30),
-                          )
-                        ],
-                      ),
-
+                        ),
+                        Text(
+                          _infoText,
+                          style: TextStyle(color: Colors.brown, fontSize: 30),
+                        )
+                      ],
+                    ),
                   ),
                 ],
               ),
